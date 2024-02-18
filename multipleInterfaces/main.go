f0dbb878d308507d9d160d9c4b443f2e386f76ef
daking24 <watcher24.inc@gmail.com>
2024-02-18 22:07:01 +0100

    Changed the cost calculation and formatting of email struct.

--- multipleInterfaces/main.go
+++ multipleInterfaces/main.go
@@ -1,38 +1,45 @@
+// Package main is the entry point of this Go program.
 package main
 
 import (
-	"fmt"
+// Importing the "fmt" package for formatted I/O.
+   "fmt"
 )
 
 func (e email) cost() int {
-	// ?
-	if e.isSubscribed == true {
-		return len(e.body) * 2
-	} else {
-		return len(e.body) * 5
-	}
+   // If the recipient is subscribed, the cost is the length of the email body multiplied by 2.
+   if e.isSubscribed == true {
+   	return len(e.body) * 2
+   // Otherwise, the cost is the length of the email body multiplied by 5.
+   } else {
+   	return len(e.body) * 5
+   }
 }
-
+// The method (e email) format() string creates a formatted string representing the email, including its body and the subscription status of the recipient.
 func (e email) format() string {
-	// ?
-	if e.isSubscribed == true {
-		msg := fmt.Sprintf("'%s' | Subscribed", e.body)
-		return msg
-	} else {
-		msg := fmt.Sprintf("'%s' | Not Subscribed", e.body)
-		return msg
-	}
+   // If the recipient is subscribed, the formatted string is "'email body' | Subscribed".
+   if e.isSubscribed == true {
+   	msg := fmt.Sprintf("'%s' | Subscribed", e.body)
+   	return msg
+   // Otherwise, the formatted string is "'email body' | Not Subscribed".
+   } else {
+   	msg := fmt.Sprintf("'%s' | Not Subscribed", e.body)
+   	return msg
+   }
 }
 
+// The type expense is an interface with a single method, cost(), which returns a float64 value.
 type expense interface {
-	cost() float64
+   cost() float64
 }
 
+// The type formatter is an interface with a single method, format(), which returns a string value.
 type formatter interface {
-	format() string
+   format() string
 }
 
+// The type email is a struct containing two fields: isSubscribed (a boolean indicating the subscription status of the recipient) and body (a string representing the email body).
 type email struct {
-	isSubscribed bool
-	body         string
+   isSubscribed bool
+   body         string
 }
